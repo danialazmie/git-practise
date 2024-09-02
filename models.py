80ad04bee3cba317528e4d3016b1c3b57a5f0a2b
danialazmie <danial.azmiee@gmail.com>
2024-09-02 13:49:58 +0800

    add BE placeholder

--- models.py
+++ models.py
@@ -1,7 +1,13 @@
+import pandas as pd
+
 class Student:
     def __init__(self, name, age, classroom):
         self.name = name
         self.age = age
         self.classroom = classroom
 
-    
+    def save(self):
+        """
+        Function to save the student data into a CSV file
+        """
+        pass
